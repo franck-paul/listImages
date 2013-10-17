e23241f4d1494132b2903feb535be09dd826ae0f
franck <carnet.franck.paul@gmail.com>
2013-10-17 08:43:26 +0200

    content_only and class widget param at the end of list

--- _widget.php
+++ _widget.php
@@ -49,7 +49,6 @@ class EntryImagesBehaviors
 		$w->EntryImages->setting('bubble',__('Image title'),1,'combo',
 			array('none' => 'none', 'image' => 'image', 'entry' => 'entry'));
 
-		$w->EntryImages->setting('class',__('CSS Class'),'','text');
 		$w->EntryImages->setting('alt',__('Alt attribute'),1,'combo',
 			array('inherit' => 'inherit', 'none' => 'none'));
 		$w->EntryImages->setting('img_dim',__('Includes width and height of image'),0,'check');
@@ -58,5 +57,6 @@ class EntryImagesBehaviors
 		$w->EntryImages->setting('length',__('Number (empty or 0 = all)'),'0');
 
 		$w->EntryImages->setting('content_only',__('Content only'),0,'check');
+		$w->EntryImages->setting('class',__('CSS Class'),'','text');
 	}
 }
