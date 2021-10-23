ebf495176a603c22ee8408b4caefa1885d8ffba1
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:44:18 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,20 +10,23 @@
  * @copyright Kozlika, Franck Paul
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "listImages",               // Name
-    "List images from entries", // Description
-    "Kozlika, Franck Paul",     // Author
+    'listImages',               // Name
+    'List images from entries', // Description
+    'Kozlika, Franck Paul',     // Author
     '1.12',                     // Version
                                 // Properties
     [
         'requires'    => [['core', '2.16']], // Dependencies
         'permissions' => 'contentadmin',
         'type'        => 'plugin',
-        'support'     => 'https://forum.dotclear.org/viewforum.php?id=16',
-        'details'     => 'https://plugins.dotaddict.org/dc2/details/listImages'
+
+        'details'    => 'https://open-time.net/?q=listImages',       // Details URL
+        'support'    => 'https://github.com/franck-paul/listImages', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/listImages/main/dcstore.xml'
     ]
 );
