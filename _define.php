e33544f59b8a1c16f1b90fa78320e42dfce65c4d
franck <carnet.franck.paul@gmail.com>
2023-05-23 16:50:47 +0200

    Release 3.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'listImages',
     'List images from entries',
     'Kozlika, Franck Paul',
-    '2.0',
+    '3.0',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
