e7f5a74985ceb21b6aaf78f27c51368a424e077f
franck <carnet.franck.paul@gmail.com>
2023-10-23 11:45:00 +0200

    Avoid magic method as far as possible

--- src/FrontendWidgets.php
+++ src/FrontendWidgets.php
@@ -40,16 +40,16 @@ class FrontendWidgets
         $params['no_content'] = false;
 
         // Récupération de la limite du nb de billets dans lesquels rechercher des images
-        $params['limit'] = abs((int) $w->limit);
+        $params['limit'] = abs((int) $w->get('limit'));
 
         // Récupération de la ou des catégories spécifiées
-        if ($w->category != '') {
-            $category          = $w->category;
+        if ($w->get('category') != '') {
+            $category          = $w->get('category');
             $params['cat_url'] = explode(',', $category);
         }
 
         // Récupération de l'indicateur de billet sélectionné
-        if ($w->selected == '1') {
+        if ($w->get('selected') == '1') {
             $params['post_selected'] = '1';
         }
 
@@ -57,19 +57,19 @@ class FrontendWidgets
         $rs = App::blog()->getPosts($params);
 
         // Récupération des options d'affichage des images
-        $size     = $w->size;
-        $html_tag = $w->html_tag;
-        $link     = $w->link;
+        $size     = $w->get('size');
+        $html_tag = $w->get('html_tag');
+        $link     = $w->get('link');
         $exif     = 0;
-        $legend   = $w->legend;
-        $bubble   = $w->bubble;
-        $from     = $w->from;
-        $start    = abs((int) $w->start);
-        $length   = abs((int) $w->length);
+        $legend   = $w->get('legend');
+        $bubble   = $w->get('bubble');
+        $from     = $w->get('from');
+        $start    = abs((int) $w->get('start'));
+        $length   = abs((int) $w->get('length'));
         $class    = $w->class;
-        $alt      = $w->alt;
-        $img_dim  = $w->img_dim;
-        $def_size = $w->def_size;
+        $alt      = $w->get('alt');
+        $img_dim  = $w->get('img_dim');
+        $def_size = $w->get('def_size');
 
         // Début d'affichage
         $ret = ($w->title ? $w->renderTitle(Html::escapeHTML($w->title)) : '');
