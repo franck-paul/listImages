1bf2ea5938801d0e79656281d130b2de3fb962da
franck <carnet.franck.paul@gmail.com>
2024-08-28 08:55:42 +0200

    New release 5.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'listImages',
     'List images from entries',
     'Kozlika, Franck Paul',
-    '5.1',
+    '5.2',
     [
         'requires'    => [['core', '2.30']],
         'permissions' => 'My',
